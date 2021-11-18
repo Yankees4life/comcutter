abecd49c1e23a41d98c26cbde7d88539f0ef745d
Yankees4life <39246923+Yankees4life@users.noreply.github.com>
2021-11-17 22:39:09 -0500

    Update comcutter.bat

--- comcutter.bat
+++ comcutter.bat
@@ -1,21 +1,21 @@
 @echo off
-set file=%1
 REM Finding commercials
-comskip --ini=comskip.ini %file%
+comskip --ini=comskip.ini %1
 REM Cutting out commercials
-copy %file%.ts video.ts
-copy %file%.ffsplit video.ffsplit
+copy %1 video.ts
+copy "%~dpn1.ffsplit" "video.ffsplit"
 for /f "usebackq tokens=*" %%a in (`bat video.ffsplit`) do ffmpeg -i video.ts %%a
 REM Merging the cuts to one file and remuxing to mp4
 for /f "usebackq tokens=*" %%i in (`fd "segment*"`) do echo file %%i >> edit.txt
 ffmpeg -f concat -i edit.txt -c copy edit.mp4
-rename edit.mp4 %file%.mp4
+rename "edit.mp4" "%~dpn1.mp4"
 REM Cleanup input files
 for /f "usebackq tokens=*" %%b in (`fd "segment*"`) do del /f %%b
 del /f edit.txt
 del /f video.ts
 del /f video.ffsplit
-del /f %file%.txt
-del /f %file%.log
-del /f %file%.ffsplit
-del /f %file%.ts
+del /f "%~dpn1.txt"
+del /f "%~dpn1.log"
+del /f "%~dpn1.ffsplit"
+del /f "%~dpn1.edl"
+del /f %1
